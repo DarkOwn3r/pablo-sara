47663b5a0807040cab1787ccf0f6b078e0ada053
Pablo Santana <pablosntojd@gmail.com>
2023-05-25 19:56:00 +0100

    refactoring kata created array of objects named data

--- katas/04-inner-html/kata.js
+++ katas/04-inner-html/kata.js
@@ -1,40 +1,57 @@
+const data = [
+    {
+        image: `https://picsum.photos/id/28/800/300`,
+        category: `travel`,
+        title: `Trip Planning for Total Beginners`,
+        content: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum amet reprehenderit nesciunt repellendus dolorem ipsa eaque repudiandae odit aperiam est assumenda, impedit alias doloremque temporibus eligendi neque, qui quas esse.`
+    },
+    {
+        image: `https://picsum.photos/id/35/800/300`,
+        category: `travel`,
+        title: `7 ways to survive in the desert`,
+        content: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum amet reprehenderit nesciunt repellendus dolorem ipsa eaque repudiandae odit aperiam est assumenda, impedit alias doloremque temporibus eligendi neque, qui quas esse.`
+    },
+    {
+        image: `https://picsum.photos/id/37/800/300`,
+        category: `travel`,
+        title: `5 amazing travel destinations`,
+        content: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum amet reprehenderit nesciunt repellendus dolorem ipsa eaque repudiandae odit aperiam est assumenda, impedit alias doloremque temporibus eligendi neque, qui quas esse.`
+    }
+]
+
+const article1 = `<article>
+<img src=${data[0].image}>
+<div class="content">
+    <p class="category">${data[0].category}</p>
+    <h2>${data[0].title}</h2>
+    <p>${data[0].content}</p>
+</div>
+</article>`
+
+const article2 = `<article>
+<img src=${data[1].image}>
+<div class="content">
+    <p class="category">${data[1].category}</p>
+    <h2>${data[1].title}</h2>
+    <p>${data[1].content}</p>
+</div>
+</article>`
+
+const article3 = `<article>
+<img src=${data[2].image}>
+<div class="content">
+    <p class="category">${data[2].category}</p>
+    <h2>${data[2].title}</h2>
+    <p>${data[2].content}</p>
+</div>
+</article>`
+
+const articles = [article1, article2, article3];
+
 const page = `<main>
-<article>
-    <img src="https://picsum.photos/id/28/800/300">
-    <div class="content">
-        <p class="category">travel</p>
-        <h2>Trip Planning for Total Beginners</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum amet reprehenderit nesciunt
-            repellendus
-            dolorem ipsa eaque repudiandae odit aperiam est assumenda, impedit alias doloremque temporibus
-            eligendi
-            neque, qui quas esse.</p>
-    </div>
-</article>
-<article>
-    <img src="https://picsum.photos/id/35/800/300">
-    <div class="content">
-        <p class="category">travel</p>
-        <h2>7 ways to survive in the desert</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum amet reprehenderit nesciunt
-            repellendus
-            dolorem ipsa eaque repudiandae odit aperiam est assumenda, impedit alias doloremque temporibus
-            eligendi
-            neque, qui quas esse.</p>
-    </div>
-</article>
-<article>
-    <img src="https://picsum.photos/id/37/800/300">
-    <div class="content">
-        <p class="category">travel</p>
-        <h2>5 amazing travel destinations</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum amet reprehenderit nesciunt
-            repellendus
-            dolorem ipsa eaque repudiandae odit aperiam est assumenda, impedit alias doloremque temporibus
-            eligendi
-            neque, qui quas esse.</p>
-    </div>
-</article>
+${article1}
+${article2}
+${article3}
 </main>`;
 
 export function kata() {
